6b1207ef59e4b672c24a4aaaa07f182b7c13fbca
acaboy <122343484+acaboy@users.noreply.github.com>
2023-01-10 14:12:59 +0700

    Add files via upload

---
+++ MESIN ATM.py
@@ -0,0 +1,20 @@
+#judul
+print("==================================")
+print("=                                 ")
+print("=           ATM BBCA              ")
+print("=                                 ")
+print("==================================")
+
+#DEKLARASI
+yangdiambil = int(input("total yang di ambil :"))
+saldo = 1000000
+sisa = saldo - yangdiambil
+
+if saldo - yangdiambil :
+    if yangdiambil <= saldo:
+        print("uang kamu", yangdiambil, "\nsisa saldo :",sisa)
+else:
+    print("maap nominal kelebnihan")
+
+
+
